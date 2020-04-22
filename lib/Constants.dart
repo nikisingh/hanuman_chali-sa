d3ce469dab3bd690964beed9b23972d3373f6483
Singh <singh.niki91@gmail.com>
2020-04-22 09:10:26 +0530

    Refactored code by adding constants and extracted common widget

---
+++ lib/Constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const kAppBarText = Text(
+  'Hanuman Chalisa',
+  style: TextStyle(
+      fontFamily: 'DancingScript',
+      fontSize: 40.0,
+      color: Colors.white,
+      //fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.italic,
+      decoration: TextDecoration.none),
+);
+
+const kFontText = TextStyle(
+    fontFamily: 'Amita',
+    fontSize: 30.0,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontStyle: FontStyle.italic,
+    decoration: TextDecoration.none);
---
+++ lib/ScreenComponents.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:hanuman_chalisa/Constants.dart';
+
+class ScreenComponents extends StatelessWidget {
+  ScreenComponents(
+      {this.size, this.image, this.text, this.prevFunction, this.nextFunction, this.pageTextNumber});
+
+  final Size size;
+  final Image image;
+  final Function prevFunction;
+  final Function nextFunction;
+  final String text;
+  final String pageTextNumber;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: Center(
+            child: image,
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Center(
+            child: Text(text, textAlign: TextAlign.center, style: kFontText),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: prevFunction,
+                child: Text('Prev'),
+              ),
+              SizedBox(width: 100, child : Text(pageTextNumber,textAlign: TextAlign.center,) , )
+              ,
+              RaisedButton(
+                onPressed: nextFunction,
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
